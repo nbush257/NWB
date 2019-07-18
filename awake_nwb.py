f6fd90a84724dd25ac5201049e4c3afffb12161b
nbush257 <nbush257@gmail.com>
2019-07-18 12:23:00 -0500

    Added a new py file with AWAKE experiment specific functions. nwb_wrapper succesfully writes a nwb from a single mat file

---
+++ awake_nwb.py
@@ -0,0 +1,214 @@
+def get_session_starttime(p):
+    '''
+    Return the start time of all the recordings for a given whisker/day
+    :param p: path of the mat files from a session
+    :return starttime: a datetime object
+    '''
+    first_file = get_first_file(p)
+    ext = os.path.splitext(first_file)[-1]
+    if ext == '.mat':
+        return(get_ddf_starttime(first_file))
+    else:
+        raise ValueError('Unsupported filetype for get_session_starttime: {}'.format(ext))
+
+
+def get_session_endtime(p):
+    '''
+    Return the end time of all the recordings for a given whisker/day
+    :param p: path of the mat files from a session
+    :return endtime: a datetime object
+    '''
+    last_file = get_last_file(p)
+    dat = load_neuromat(os.path.join(p,last_file))
+    finfo = dat.fileInfo
+    starttime = datetime(finfo.Time_Year,finfo.Time_Month,finfo.Time_Day,finfo.Time_Hour,finfo.Time_Min,finfo.Time_Sec,tzinfo=tzlocal())
+    dur = finfo.TimeSpan
+    endtime = starttime + timedelta(seconds=dur)
+    return(endtime)
+
+
+def get_first_file(p,fullfile_tgl=True):
+    '''
+    Get the first file in a directory. SHould be mat files converted from DDFs
+    :param p: directory to search
+    :return first_file: the first file in the directory (alphabetical order)
+    '''
+    file_list = glob.glob(os.path.join(p,'*.mat'))
+    if not fullfile_tgl:
+        first_file = os.path.split(file_list[0])[1]
+    else:
+        first_file = file_list[0]
+    return(first_file)
+
+
+def get_last_file(p):
+    '''
+    Get the first file in a directory. SHould be mat files converted from DDFs
+    :param p: directory to search
+    :return first_file: the first file in the directory (alphabetical order)
+    '''
+    file_list = glob.glob(os.path.join(p,'*.mat'))
+    last_file = os.path.split(file_list[-1])[1]
+    return(last_file)
+
+
+def sanitize_AWAKE_filename(f):
+    '''
+    takes a filename and makes sure it follows the expected format.
+    Then strips all the needed metadata
+    :param f: a filename from the matlab conversion script
+    :return meta: a dict with metadata stripped from the filename
+    '''
+    f = os.path.split(f)[-1]
+    if f[0]!='m':
+        raise ValueError('File does not start with "m". Is this a valid mouse file?')
+
+    fileparts = re.split('[_\-]',f)
+    if len(fileparts) !=5:
+        raise ValueError('Filename does not have the 5 expected parts.')
+    if fileparts[-1] != 'tempConvert.mat':
+        raise ValueError('filename does not end with tempConvert indicator')
+    if len(fileparts[0])!= 7:
+        raise ValueError('Mouse number is wrong length')
+    if len(fileparts[1])!= 8:
+        raise ValueError('Recording date is {}, and is of wrong length'.format(fileparts[1]))
+    if len(fileparts[2])!= 2:
+        raise ValueError('Whisker ID is wrong length')
+    mouse_num = fileparts[0]
+    rec_date = fileparts[1]
+    whisker = fileparts[2]
+    trial = int(fileparts[3])
+
+    d_out =  {'whisker':whisker,
+              'mouse_num':mouse_num,
+              'trial_num':trial,
+              'rec_date':rec_date,
+              'ID':'_'.join([mouse_num,whisker,rec_date,'t{:02d}'.format(trial)])}
+
+    return(d_out)
+
+
+def mark_positions(motor,nwb_file):
+    '''
+    Add the times during which the motor was in a given position
+    i.e., exclude times when motor is moving.
+    :param motor: The digital read from the motor
+    :param nwb_file: the NWB file object to write to
+    :return:
+    '''
+    temp = np.concatenate([[0.],motor.timestamps])
+    position_times = np.reshape(temp[:-1],[-1,2]) # marks the motor motion time
+    for ii,(onset,offset) in enumerate(position_times):
+        nwb_file.add_epoch(onset,offset,'pos_{0:03d}'.format(ii))
+
+
+def add_neural_analog(f,nwb_file):
+    if os.path.splitext(f)[-1]=='.mat':
+        get_AWAKE_neural(f,nwb_file)
+    else:
+        raise ValueError('Filetype not supportd to add analog signal: {}'.format(os.path.splitext(f)[-1]))
+
+
+def get_AWAKE_neural(ddf,nwb_file,gain=10000):
+    '''
+    extract the neural data from a matlab ddf. Assumes one channel of recording,
+    and that the one channel has a specific format in the matlab ddf.
+    :param f: a matlab ddf
+    :param conversion: the gain of the amplifier
+    :return ephys_ts: a NWB ElectricalSeries object
+    '''
+    dat = overload_ddf(ddf)
+    electrode_table_region = nwb_file.create_electrode_table_region([0],
+                                                                    'this is hardcoded for one electrode') # currently hardcoded for 1 electrode
+
+    # get the time difference between the start of the session and the start of this recording
+
+    offset_time = get_offset_time(dat,nwb_file)
+    # create the ephys object and add it to the nwb file
+    ephys_ts = ElectricalSeries('channel_0',
+                                dat.analogData.Neural,
+                                electrode_table_region,
+                                timestamps=dat.time+offset_time,
+                                conversion=1/float(gain)
+                                )
+    nwb_file.add_acquisition(ephys_ts)
+    return(ephys_ts)
+
+
+def add_camera_trigger(ddf,nwb_file):
+    '''
+    use this to transform the camera trigger analog input into events
+    :param ddf:
+    :param nwb_file:
+    :return:
+    '''
+
+    dat = overload_ddf(ddf)
+    offset_time = get_offset_time(dat,nwb_file)
+    idx = trigger_to_idx(dat.analogData.Cam_trig)
+    ts = dat.time[idx]
+    frametimes = pynwb.ecephys.TimeSeries('Camera Frame onsets',
+                                          idx,
+                                          'indices',
+                                          timestamps=ts+offset_time)
+    nwb_file.add_acquisition(frametimes)
+    return(frametimes)
+
+
+def trigger_to_idx(trigger,thresh=100.,sign='+'):
+    '''
+    Takes an analog signal of a camera trigger and finds the
+    when the trigger crosses.
+    :param trigger:
+    :param thresh:
+    :return idx: indices in the analog signal in which the threshold was croseed
+    '''
+    # get the initial value of the trigger, along with the 10th percentile lagrest and smallest values
+    initial = np.median(trigger[:100])
+    biggest = np.quantile(trigger,.9)
+    smallest = np.quantile(trigger,.1)
+
+    if (biggest-smallest) <1000:
+        raise ValueError('Difference between big and small values in camera trigger is less than 1000')
+    thresh = np.mean([biggest,smallest])
+
+    above = trigger>thresh
+    d = np.diff(above.astype('double'))
+    first = np.where(np.abs(d)>.5)[0][0]
+    if d[first]>0:
+        idx =np.where(d>.5)[0]
+    else:
+        idx =np.where(d<-.5)[0]
+
+    frame_interval = np.mean(np.diff(idx))/40000. #calculate the frame interval in seconds assuming a 40kHz sample
+    print('='*15)
+    print('Frame rate of camera is calculated to be {:.2f} fps\n'.format(frame_interval**-1))
+
+    return(idx)
+
+
+def add_trial_data(nwb_file,cat_dict):
+    '''
+    Add information on when trials occured into the nwb file
+    :param nwb_file: file to add trial information to
+    :param cat_dict: dict returned when you concatenated the ddfs
+    :return: None
+    '''
+    #TODO: import trial information from external source
+    for trial_start,dur in zip(cat_dict['recording_start_times'],cat_dict['recording_lengths']):
+        nwb_file.add_trial(start_time=trial_start,stop_time=trial_start+dur)
+
+def create_AWAKW():
+
+    ephys_ts = ElectricalSeries('channel_0',
+                                cat_dict['neural'],
+                                electrode_table_region,
+                                timestamps=cat_dict['ts'],
+                                conversion=1./float(gain))
+    nwb_file.add_acquisition(ephys_ts)
+    cam_trig = TimeSeries('Camera frame onsets',
+                          cat_dict['frame_idx'],
+                          'indices',
+                          timestamps=cat_dict['frame_times'])
+    nwb_file.add_acquisition(cam_trig)
+    add_trial_data(nwb_file,cat_dict)
--- nwb_wrapper.py
+++ nwb_wrapper.py
@@ -43,20 +43,6 @@ def overload_ddf(ddf):
     return(dat)
 
 
-def get_session_starttime(p):
-    '''
-    Return the start time of all the recordings for a given whisker/day
-    :param p: path of the mat files from a session
-    :return starttime: a datetime object
-    '''
-    first_file = get_first_file(p)
-    ext = os.path.splitext(first_file)[-1]
-    if ext == '.mat':
-        return(get_ddf_starttime(first_file))
-    else:
-        raise ValueError('Unsupported filetype for get_session_starttime: {}'.format(ext))
-
-
 def get_ddf_starttime(ddf):
     '''
     Specific start time get for the ddf mat files
@@ -69,46 +55,6 @@ def get_ddf_starttime(ddf):
     starttime = starttime-timedelta(seconds=dat.fileInfo.TimeSpan)
 
     return(starttime)
-
-
-def get_session_endtime(p):
-    '''
-    Return the end time of all the recordings for a given whisker/day
-    :param p: path of the mat files from a session
-    :return endtime: a datetime object
-    '''
-    last_file = get_last_file(p)
-    dat = load_neuromat(os.path.join(p,last_file))
-    finfo = dat.fileInfo
-    starttime = datetime(finfo.Time_Year,finfo.Time_Month,finfo.Time_Day,finfo.Time_Hour,finfo.Time_Min,finfo.Time_Sec,tzinfo=tzlocal())
-    dur = finfo.TimeSpan
-    endtime = starttime + timedelta(seconds=dur)
-    return(endtime)
-
-
-def get_first_file(p,fullfile_tgl=True):
-    '''
-    Get the first file in a directory. SHould be mat files converted from DDFs
-    :param p: directory to search
-    :return first_file: the first file in the directory (alphabetical order)
-    '''
-    file_list = glob.glob(os.path.join(p,'*.mat'))
-    if not fullfile_tgl:
-        first_file = os.path.split(file_list[0])[1]
-    else:
-        first_file = file_list[0]
-    return(first_file)
-
-
-def get_last_file(p):
-    '''
-    Get the first file in a directory. SHould be mat files converted from DDFs
-    :param p: directory to search
-    :return first_file: the first file in the directory (alphabetical order)
-    '''
-    file_list = glob.glob(os.path.join(p,'*.mat'))
-    last_file = os.path.split(file_list[-1])[1]
-    return(last_file)
 
 
 def get_subject_info(csv_file,id):
@@ -128,20 +74,22 @@ def get_subject_info(csv_file,id):
     return(pynwb.file.Subject(**sub_data))
 
 
-def init_NWB(p,yaml_file):
+def init_NWB(f,subject,yaml_file):
     '''
     Initialize an NWB file with appropriate metadata
     :param p: path where all the matlab files exist to be appended to an NWB file
     :return:
     '''
-    first_file = get_first_file(p)
-    fname_meta = sanitize_AWAKE_filename(first_file)
-    ID = fname_meta['ID']
-    starttime = get_session_starttime(p)
     exp_params = import_NWB_params(yaml_file)
-    exp_params['subject']=pynwb.file.Subject(subject_id=fname_meta['mouse_num'],genotype='c57bl6')
+
+    if type(subject) is dict:
+        exp_params['subject']=pynwb.file.Subject(subject_id=subject['ID'],genotype=subject['genotype'])
+        ID = subject['ID']
+    elif type(subject) is pynwb.file.Subject:
+        ID = subject.subject_id
+
     nwb_file = NWBFile(exp_params.pop('desc'),ID,
-                  session_start_time=starttime,
+                  session_start_time=get_ddf_starttime(f),
                   file_create_date=datetime.now(),
                   **exp_params)
     return(nwb_file)
@@ -175,43 +123,7 @@ def init_nwb_electrode(electrode_yaml,nwb_file):
                                group=group)
 
 
-def sanitize_AWAKE_filename(f):
-    '''
-    takes a filename and makes sure it follows the expected format.
-    Then strips all the needed metadata
-    :param f: a filename from the matlab conversion script
-    :return meta: a dict with metadata stripped from the filename
-    '''
-    f = os.path.split(f)[-1]
-    if f[0]!='m':
-        raise ValueError('File does not start with "m". Is this a valid mouse file?')
-
-    fileparts = re.split('[_\-]',f)
-    if len(fileparts) !=5:
-        raise ValueError('Filename does not have the 5 expected parts.')
-    if fileparts[-1] != 'tempConvert.mat':
-        raise ValueError('filename does not end with tempConvert indicator')
-    if len(fileparts[0])!= 7:
-        raise ValueError('Mouse number is wrong length')
-    if len(fileparts[1])!= 8:
-        raise ValueError('Recording date is {}, and is of wrong length'.format(fileparts[1]))
-    if len(fileparts[2])!= 2:
-        raise ValueError('Whisker ID is wrong length')
-    mouse_num = fileparts[0]
-    rec_date = fileparts[1]
-    whisker = fileparts[2]
-    trial = int(fileparts[3])
-
-    d_out =  {'whisker':whisker,
-            'mouse_num':mouse_num,
-            'trial_num':trial,
-            'rec_date':rec_date,
-              'ID':'_'.join([mouse_num,whisker,rec_date,'t{:02d}'.format(trial)])}
-
-    return(d_out)
-
-
-def add_all_analog(ddf,nwb_file,starting_time):
+def add_all_analog(ddf,nwb_file):
     '''
     Add all the analog data and information to an NWB as a
     NWB TimeSeries object
@@ -222,12 +134,12 @@ def add_all_analog(ddf,nwb_file,starting_time):
     '''
     dat = overload_ddf(ddf)
     for label in dat.analogInfo._fieldnames:
-        TS = get_analog_TS(dat,label,starting_time)
+        TS = get_analog_TS(dat,label)
 
-    nwb_file.add_acquisition(TS)
+        nwb_file.add_acquisition(TS)
 
 
-def get_analog_TS(dat,label,starting_time):
+def get_analog_TS(dat,label):
     '''
     Converts an individual analog trace from the datawave matlab format to
     the NWB timeseries base class.
@@ -245,8 +157,7 @@ def get_analog_TS(dat,label,starting_time):
                                data=data,
                                unit=info.Units,
                                resolution=info.Resolution,
-                               timestamps=dat.time,
-                               starting_time=starting_time)
+                               timestamps=dat.time)
     return(TS)
 
 
@@ -264,12 +175,11 @@ def convert_TS_to_ES(TS,electrode_table_region,gain=10000):
                                               unit=TS.unit,
                                               resolution=TS.Resolution,
                                               timestamps=TS.timestamps,
-                                              starting_time=TS.starting_time,
                                               electrode_table_region=electrode_table_region)
     return(ephys_ts)
 
 
-def add_all_digital(ddf,nwb_file,starting_time):
+def add_all_digital(ddf,nwb_file):
     '''
     Add all the analog data and information to an NWB as a
     NWB TimeSeries object
@@ -280,12 +190,11 @@ def add_all_digital(ddf,nwb_file,starting_time):
     '''
     dat = overload_ddf(ddf)
     for label in dat.eventData._fieldnames:
-        AS = get_digital_AS(dat,label,starting_time)
+        AS = get_digital_AS(dat,label)
+        nwb_file.add_acquisition(AS)
 
-    nwb_file.add_acquisition(AS)
 
-
-def get_digital_AS(dat,label,starting_time):
+def get_digital_AS(dat,label):
     '''
     Converts an individual digital trace from the datawave matlab format to
     the annotation series class
@@ -295,116 +204,21 @@ def get_digital_AS(dat,label,starting_time):
 
     :return: AS - a NWB TimeSeries object with the ddf data populated
     '''
+    print('Convertiong {}'.format(label))
     data = dat.eventData.__getattribute__(label)
+    if type(data.val) is not np.array:
+        data.val = np.array([data.val])
+        data.ts = np.array([data.ts])
 
     AS = pynwb.misc.AnnotationSeries(name=label,
                                      data=data.val,
-                                     timestamps=data.ts+starting_time,
-                                     )
+                                     timestamps=data.ts)
     return(AS)
-
-
-def mark_positions(motor,nwb_file):
-    '''
-    Add the times during which the motor was in a given position
-    i.e., exclude times when motor is moving.
-    :param motor: The digital read from the motor
-    :param nwb_file: the NWB file object to write to
-    :return:
-    '''
-    temp = np.concatenate([[0.],motor.timestamps])
-    position_times = np.reshape(temp[:-1],[-1,2]) # marks the motor motion time
-    for ii,(onset,offset) in enumerate(position_times):
-        nwb_file.add_epoch(onset,offset,'pos_{0:03d}'.format(ii))
-
-
-def add_neural_analog(f,nwb_file):
-    if os.path.splitext(f)[-1]=='.mat':
-        get_AWAKE_neural(f,nwb_file)
-    else:
-        raise ValueError('Filetype not supportd to add analog signal: {}'.format(os.path.splitext(f)[-1]))
-
-
-def get_AWAKE_neural(ddf,nwb_file,gain=10000):
-    '''
-    extract the neural data from a matlab ddf. Assumes one channel of recording,
-    and that the one channel has a specific format in the matlab ddf.
-    :param f: a matlab ddf
-    :param conversion: the gain of the amplifier
-    :return ephys_ts: a NWB ElectricalSeries object
-    '''
-    dat = overload_ddf(ddf)
-    electrode_table_region = nwb_file.create_electrode_table_region([0],
-                                'this is hardcoded for one electrode') # currently hardcoded for 1 electrode
-
-    # get the time difference between the start of the session and the start of this recording
-
-    offset_time = get_offset_time(dat,nwb_file)
-    # create the ephys object and add it to the nwb file
-    ephys_ts = ElectricalSeries('channel_0',
-                                dat.analogData.Neural,
-                                electrode_table_region,
-                                timestamps=dat.time+offset_time,
-                                conversion=1/float(gain)
-                                )
-    nwb_file.add_acquisition(ephys_ts)
-    return(ephys_ts)
-
-
-def add_camera_trigger(ddf,nwb_file):
-    '''
-    use this to transform the camera trigger analog input into events
-    :param ddf:
-    :param nwb_file:
-    :return:
-    '''
-
-    dat = overload_ddf(ddf)
-    offset_time = get_offset_time(dat,nwb_file)
-    idx = trigger_to_idx(dat.analogData.Cam_trig)
-    ts = dat.time[idx]
-    frametimes = pynwb.ecephys.TimeSeries('Camera Frame onsets',
-                                          idx,
-                                          'indices',
-                                          timestamps=ts+offset_time)
-    nwb_file.add_acquisition(frametimes)
-    return(frametimes)
-
-
-def trigger_to_idx(trigger,thresh=100.,sign='+'):
-    '''
-    Takes an analog signal of a camera trigger and finds the
-    when the trigger crosses.
-    :param trigger:
-    :param thresh:
-    :return idx: indices in the analog signal in which the threshold was croseed
-    '''
-    # get the initial value of the trigger, along with the 10th percentile lagrest and smallest values
-    initial = np.median(trigger[:100])
-    biggest = np.quantile(trigger,.9)
-    smallest = np.quantile(trigger,.1)
-
-    if (biggest-smallest) <1000:
-        raise ValueError('Difference between big and small values in camera trigger is less than 1000')
-    thresh = np.mean([biggest,smallest])
-
-    above = trigger>thresh
-    d = np.diff(above.astype('double'))
-    first = np.where(np.abs(d)>.5)[0][0]
-    if d[first]>0:
-        idx =np.where(d>.5)[0]
-    else:
-        idx =np.where(d<-.5)[0]
-
-    frame_interval = np.mean(np.diff(idx))/40000. #calculate the frame interval in seconds assuming a 40kHz sample
-    print('='*15)
-    print('Frame rate of camera is calculated to be {:.2f} fps\n'.format(frame_interval**-1))
-
-    return(idx)
 
 
 def get_offset_time(ddf,nwb_file):
     '''
+    [deprecated?]
     Utility function to get a given recordings offset from the first recording of the session
     :param ddf: a ddf file or data
     :param nwb_file: the nwb_file to append to
@@ -418,6 +232,8 @@ def get_offset_time(ddf,nwb_file):
 
 
 def concatenate_recordings(p):
+    #[DEPRECATED]
+    pass
 
     '''
     concatenate all the recordings in a given path
@@ -491,45 +307,35 @@ def concatenate_recordings(p):
     return(cat_dict)
 
 
-def add_trial_data(nwb_file,cat_dict):
+def write_NWB(f,exp_yaml,subject=None,NWBfilename=None,gain=10000.):
     '''
-    Add information on when trials occured into the nwb file
-    :param nwb_file: file to add trial information to
-    :param cat_dict: dict returned when you concatenated the ddfs
-    :return: None
-    '''
-    #TODO: import trial information from external source
-    for trial_start,dur in zip(cat_dict['recording_start_times'],cat_dict['recording_lengths']):
-        nwb_file.add_trial(start_time=trial_start,stop_time=trial_start+dur)
-
-
-def write_AWAKE_NWB(p,exp_yaml,electrode_yaml,gain=10000.):
-    '''
-    Create an NWB file for the all the recordings of a particular session for the AWAKE data
-    :param p: path where all the ddf-mat files live
+    convert a Matlab DDF to a NWB formatted file
+    :param f: DDF file to convert
     :param exp_yaml: a yaml which holds metadata for the experiment
+    :param electrode_yaml:a yaml which holds metadata for the experiment
     :return:
     '''
-    first_file = get_first_file(p)
-    fname_meta = sanitize_AWAKE_filename(first_file)
-    NWBfilename = os.path.join(p,fname_meta['mouse_num'] + '_' + fname_meta['rec_date'] + '_' + fname_meta['whisker']+'.nwb')
-    cat_dict = concatenate_AWAKE_recordings(p)
-    nwb_file = init_NWB(p,exp_yaml)
-    init_nwb_electrode(electrode_yaml,nwb_file)
-    electrode_table_region = nwb_file.create_electrode_table_region([0],
-                                                                    'this is hardcoded for one electrode') # currently hardcoded for 1 electrode
-    ephys_ts = ElectricalSeries('channel_0',
-                                cat_dict['neural'],
-                                electrode_table_region,
-                                timestamps=cat_dict['ts'],
-                                conversion=1./float(gain))
-    nwb_file.add_acquisition(ephys_ts)
-    cam_trig = TimeSeries('Camera frame onsets',
-                          cat_dict['frame_idx'],
-                          'indices',
-                          timestamps=cat_dict['frame_times'])
-    nwb_file.add_acquisition(cam_trig)
-    add_trial_data(nwb_file,cat_dict)
+    p = os.path.split(f)[0]
+
+    # If no output name is supplied. simply replace the extension with nwb
+    if NWBfilename is None:
+        NWBfilename = os.path.splitext(f)[0]+'.nwb'
+
+    # If no subject info is given, ask user for ID and genotype
+    if subject is None:
+        subject={}
+        subject['ID'] = input('Enter the subject ID number')
+        subject['genotype'] = input('Enter the subject genotype (default=c57bl6)')
+        if len(subject['genotype'])==0:
+            subject['genotype'] = 'c57bl6'
+
+
+    # Load matlab DDF and add all entries
+    dat = overload_ddf(f)
+    nwb_file = init_NWB(f,subject,exp_yaml)
+    add_all_analog(dat,nwb_file)
+    add_all_digital(dat,nwb_file)
+
     # write the file
     print('writing NWB_file to\n\t{}...'.format(NWBfilename))
     with NWBHDF5IO('{}'.format(NWBfilename), 'w') as io:
@@ -537,3 +343,5 @@ def write_AWAKE_NWB(p,exp_yaml,electrode_yaml,gain=10000.):
     print('Done!')
 
 
+#TODO: make able to read subject as a csv
+#TODO: test conversion with subject as csv and as empty
